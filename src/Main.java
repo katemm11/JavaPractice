bb16e293c0f84da3c80ae2d41681f1a3724c04c9
Murray <KATE.MURRAY@VIZIENTINC.COM>
2020-04-13 16:17:10 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+
+
+    /*
+    This is a block
+    comment
+    //Can put line comments in the middle of block comments
+   */
+
+
+
+
+    public static void main(String[] args) {
+	System.out.println("Hello World One"); //This is a comment
+	System.out.println("Hello World Two");
+	System.out.println("Hello World Three");
+    }
+}
